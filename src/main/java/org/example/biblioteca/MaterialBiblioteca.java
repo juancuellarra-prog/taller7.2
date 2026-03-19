b370ae6195aeaf061cd270c16244efe8a9c5e6d6
juan jose cuellar <juan_cuellarra@fet.edu.co>
2026-03-18 20:09:49 -0500

    holaaa

---
+++ src/main/java/org/example/biblioteca/MaterialBiblioteca.java
@@ -0,0 +1,7 @@
+package org.example.biblioteca;
+
+public class titulo extends MaterialBiblioteca{
+    public titulo(String titulo,String autor, int año) {
+        super(titulo,autor,año);
+    }
+}
---
+++ src/main/java/org/example/biblioteca/Tesis.java
@@ -0,0 +1,21 @@
+package org.example.biblioteca;
+
+public class Tesis extends MaterialBiblioteca {
+
+    private String universidad;
+
+    public Tesis() {
+    }
+
+    public Tesis(String titulo, String autor, int anio, String universidad) {
+        super(titulo, autor, anio);
+        this.universidad = universidad;
+    }
+
+    @Override
+    public void mostrarInfo() {
+        super.mostrarInfo();
+        System.out.println("Universidad: " + universidad);
+        System.out.println("------------------");
+    }
+}
